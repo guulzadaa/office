c93448c170aad3a74fcdce5817cbf9595877b006
Issa Gulzada <242194@astanait.edu.kz>
2024-12-26 11:00:10 +0500

    Created Person class

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+//Task 1
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+}
