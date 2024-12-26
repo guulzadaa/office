e3bd82a76b0e29035562ffaa46b15de885a00585
Issa Gulzada <242194@astanait.edu.kz>
2024-12-26 13:44:54 +0500

    Created Person class

--- src/Person.java
+++ src/Person.java
@@ -12,5 +12,44 @@ public class Person {
         this.age = age;
         this.gender = gender;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
+
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public boolean isGender() {
+        return gender;
+    }
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String toString() {
+        String genderStr = gender ? "Male" : "Female";
+        return "Hi, I am" + name + " " + surname + ", a" + age + "-year-old " + genderStr + ".";
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Gulzada", "Issa", 18, false);
+        System.out.println(person);
+    }
 }
